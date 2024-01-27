94506836a2adf4932738cc24e137bcd8c285c906
EWANZO101 <107235474+EWANZO101@users.noreply.github.com>
2024-01-27 22:29:20 +0000

    updat.bat update ..

--- caddownloads/update(2).bat
+++ caddownloads/update(2).bat
@@ -7,5 +7,19 @@ git stash && git pull origin main
 pnpm install
 pnpm run build
 
-echo  All processes are completed. Please close the command prompt !!.
+echo All processes are completed.
+
+pause
+
+set /p user_input=Do you want to open start.bat? (y/n): 
+if /i "%user_input%"=="y" (
+    start "" "C:\Users\Administrator\Documents\snaily-cadv4\start.bat"
+) else if /i "%user_input%"=="n" (
+    echo Updater closed.
+) else (
+    echo Invalid input. Updater closed.
+)
+
+echo Press Enter to close the prompt.
+pause >nul
 exit
