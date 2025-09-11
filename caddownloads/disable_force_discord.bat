32b19f5a9d687bb6f570a4965e5f508943c26649
EWANZO101 <107235474+EWANZO101@users.noreply.github.com>
2025-09-11 08:47:16 +0100

    Update disable_force_discord.bat

--- caddownloads/disable_force_discord.bat
+++ caddownloads/disable_force_discord.bat
@@ -1,25 +1,28 @@
 @echo off
 setlocal
 
-:: Prompt for PostgreSQL password
+:: Prompt for PostgreSQL version, username, database, and password
+set /p PG_VERSION=Enter PostgreSQL version (e.g., 15 u can find this in C:\Program Files\PostgreSQL\): 
+set /p DB_USER=Enter PostgreSQL username: 
+set /p DB_NAME=Enter PostgreSQL database name: 
 set /p db_password=Enter PostgreSQL password (it will be visible): 
-
-:: Set environment variables
-set DB_NAME=snaily-cad-v4
-set DB_USER=postgres
 
 :: Set the password for psql
 set PGPASSWORD=%db_password%
 
+:: Build full path to psql.exe based on version
+set PSQL_PATH="C:\Program Files\PostgreSQL\%PG_VERSION%\bin\psql.exe"
+
 echo Disabling FORCE_DISCORD_AUTH in SnailyCAD...
 
 :: Run the SQL
-psql -U %DB_USER% -d %DB_NAME% -c "UPDATE public.\"CadFeature\" SET \"isEnabled\" = false WHERE feature = 'FORCE_DISCORD_AUTH';"
-psql -U %DB_USER% -d %DB_NAME% -c "SELECT feature, \"isEnabled\" FROM public.\"CadFeature\" WHERE feature = 'FORCE_DISCORD_AUTH';"
+%PSQL_PATH% -U %DB_USER% -d %DB_NAME% -c "UPDATE public.\"CadFeature\" SET \"isEnabled\" = false WHERE feature = 'FORCE_DISCORD_AUTH';"
+%PSQL_PATH% -U %DB_USER% -d %DB_NAME% -c "SELECT feature, \"isEnabled\" FROM public.\"CadFeature\" WHERE feature = 'FORCE_DISCORD_AUTH';"
 
 :: Cleanup
-set PGPASSWORD=
+set "PGPASSWORD="
 
 echo ✅ FORCE_DISCORD_AUTH has been disabled.
-pause
+
 endlocal
+pause
